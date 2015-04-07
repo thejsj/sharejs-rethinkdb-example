3c3dcb69bcc945561c36a819448197d568843d86
thejsj <jorge.silva@thejsj.com>
2015-04-06 23:27:42 -0700

    Add CodeMiror example

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var shareJSServer = require('./sharejs-server');
+var shareCodeMirror = require('share-codemirror');
 
 // Init app
 var app = express();
 
 app
+  .use(express.static(shareCodeMirror.scriptsDir))
   .use(express.static(__dirname + '/../client'))
   .listen(8000, function () {
     console.log('Server listening on port:', 8000);
